1cb877cedc2df7fc3d465813ce1cd09d02a6d240
Doggies-Galore <68348295+Doggies-Galore@users.noreply.github.com>
2023-03-30 11:47:15 -0500

    Update Engine.py

--- Engine.py
+++ Engine.py
@@ -2,13 +2,6 @@
 #Why are you snooping in a fast food chain's framework... like man, seriously!
 import time
 #inhr34gkfbipi4wrjnbio3NRI#JOV1BNVJUBNJB#N$JBNOIJ$B#N@JNO$@#BNOBN$@TVOBNO$IT@NBOVE$TF@NBIVNPI$BN@IGUVPOIU$BNT@FG
-for x in "":
-    while True:
-      print("ejgoneijwrtbgnijbnqiUNBUI#NBUIORBNPUIHN#RUIHNGOUHN#R!UIOGHNOUI#RHN!OIUGHNIOU#HNROGIUHNOIuR#HNOIUGBNRPOU#BRUOGHOIUH#PROIUGHPOUI#!RHGOUHO#RIUJOIUH#R!IUJIOUH#!JOIGHJOI#HJ!GGOIJHROI!HJOIHJ!OIUHJVOIUHJROUHJGVOI!HJGVIOUHJR!OIHJROP!hjR!OPIR!HJOIHJO#IRH!OUR@B")
-      #uHOU#H@OUGHUOIHGNO#IGNOIPUHGNI#OUHPIOUBHNIOU#HNOIH!OBRopN#BIORO#
-      #!O#JGNh3iogjhoijH#IOHJI(JH#RI(HJGIJH#RIO%!HJoih3j4oihjgOI#HR!OIHoi
-        if 9 == 10-1:
-          #w4tngonbiouqBN$@TUION#QOJBNV#$2obnVPOIQ@#$JNghooup4b2np3NHOUGN$@#IOJNGIOPJN@
 print("Welcome to Burger Castle. Give us one moment while we download our menu...")
 import os
 #3qrkgkqo34nrgo[nqoi234ngioBNOBN$@TVOBNO$IT@NBOVE$TF@oi3qrjg3qorgnqo34nrgojn3r2jo
@@ -21,35 +14,11 @@ bag=[]
 #ortngounw4topbfg4q2netbgho
 #i4gptbn4weigtbnpin4wtgjnopiw4nqg2ntqw4bgntop
 #iwn3bgifbn3i2bgni3bnipgtnbpijo3rn3og;jno3;q2nwgjktrnlq32rgt
-for x in "":
-  #n4giuopnripuo3bnruoibn34ripuobnq234griobnq3ripgbnopiqn34rogpnq43oer
-  #in4grionr4q3ubgtohqn34ouighntfiuoq3bn4ruginq3gtfr3qgf3iuhbtgnijok
-  #4tiwgnhiow4ng5ionpow34ngtiopnhq2po34gnftioq3ngriotq3oibnwrtgfoibq3nwro
-  while True:
-    print("ejg3fureqbnregiubniubqew4ugnoiqe4nbrqe3grfq3rijofgnbiq3prgtbfijpqb3rg#BRUOGHOIUH#PROIUGHPOUI#!RHGOUHO#RIUJOIUH#R!IUJIOUH#!JOIGHJOI#HJ!GGOIJHROI!HJOIHJ!OIUHJVOIUHJROUHJGVOI!HJGVIOUHJR!OIHJROP!hjR!OPIR!HJOIHJO#IRH!OUR@B")
-    #uHOU#H@OUGHUOIHGNO#IGNOIPUHGNI#OUHPIOUBHNIOU#HNOIH!OBRopN#BIORO#
-    #!O#JGNh3iogjhoijH#IOHJI(JH#RI(HJGIJH#RIO%!HJoih3j4oihjgOI#HR!OIHoi
-    if 11111111 == 11111112-1:
-      #w4tngonbiouqBN$@TUION#QOJBNV#$2obnVPOIQ@#$JNghooup4b2np3NHOUGN$@#IOJNGIOPJN@
-      while True:
-        print("fngq3o4ngoiftnq234grtin42pi3bgntpiufbn42q3infghiu4btniub3ripubgthi4bhwgtnwi4r")
 os.system("wget -q https://raw.githubusercontent.com/Doggies-Galore/Magic8Ball/main/menu.acr")
 #3qrkgkqo34nrgo[nqoi234ngioBNOBN$@TVOBNO$IT@NBOVE$TF@oi3qrjg3qorgnqo34nrgojn3r2jo
 #ortngounw4topbfg4q2netbgho
 #i4gptbn4weigtbnpin4wtgjnopiw4nqg2ntqw4bgntop
 #iwn3bgifbn3i2bgni3bnipgtnbpijo3rn3og;jno3;q2nwgjktrnlq32rgt
-for x in "":
-    #n4giuopnripuo3bnruoibn34ripuobnq234griobnq3ripgbnopiqn34rogpnq43oer
-    #in4grionr4q3ubgtohqn34ouighntfiuoq3bn4ruginq3gtfr3qgf3iuhbtgnijok
-    #4tiwgnhiow4ng5ionpow34ngtiopnhq2po34gnftioq3ngriotq3oibnwrtgfoibq3nwro
-    while True:
-        print("ejg3fureqbnregiubniubqew4ugnoiqe4nbrqe3grfq3rijofgnbiq3prgtbfijpqb3rg#BRUOGHOIUH#PROIUGHPOUI#!RHGOUHO#RIUJOIUH#R!IUJIOUH#!JOIGHJOI#HJ!GGOIJHROI!HJOIHJ!OIUHJVOIUHJROUHJGVOI!HJGVIOUHJR!OIHJROP!hjR!OPIR!HJOIHJO#IRH!OUR@B")
-        #uHOU#H@OUGHUOIHGNO#IGNOIPUHGNI#OUHPIOUBHNIOU#HNOIH!OBRopN#BIORO#
-        #!O#JGNh3iogjhoijH#IOHJI(JH#RI(HJGIJH#RIO%!HJoih3j4oihjgOI#HR!OIHoi
-        if 11111111 == 11111112-1:
-            #w4tngonbiouqBN$@TUION#QOJBNV#$2obnVPOIQ@#$JNghooup4b2np3NHOUGN$@#IOJNGIOPJN@
-            while True:
-                print("fngq3o4ngoiftnq234grtin42pi3bgntpiufbn42q3infghiu4btniub3ripubgthi4bhwgtnwi4r")
 import subprocess as sp
 menu=sp.getoutput("cat menu.acr")
 print("Here's one we have to offer:")
